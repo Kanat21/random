f175f39d8ffa51b0ff2a56dc8aee969633d341d7
Kanat21 <turdumametovnursultan427@gmail.com>
2022-01-06 22:09:08 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+
+            final double max = 100.; // Максимальное число для диапазона от 0 до max
+            final double rnd = rnd(max);
+
+            System.out.println("Псевдослучайное вещественное число: " + rnd);
+        }
+
+
+
+
+
+    private static double rnd(double max) {
+        return max;
+    }
+}
